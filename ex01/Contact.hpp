433773e7972b19434034aba1b29f198c1cd66690
jlupito <jl.arthaud@protonmail.com>
2023-10-31 18:10:28 +0100

    first ex done

---
+++ ex01/Contact.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jarthaud <jarthaud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 14:42:43 by jarthaud          #+#    #+#             */
+/*   Updated: 2023/10/31 17:35:13 by jarthaud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_H
+# define CONTACT_H
+
+class Contact{
+    public:
+    
+    
+    private:
+    std::string _firstName;
+    std::string _lastName;
+    std::string _nickName;
+    std::string _phoneNumber;
+    std::string _darkSecret;
+}
+
+#endif
---
+++ ex01/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jarthaud <jarthaud@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/31 15:11:08 by jarthaud          #+#    #+#             */
+/*   Updated: 2023/10/31 17:56:25 by jarthaud         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "PhoneBook.hpp"
+#include "contact.hpp"
+
+int main()
+{
+    PhoneBook book;
+    std::string input = "";
+    
+    std::cout << "Please select ADD, SEARCH or EXIT" << std::endl;
+    std::cout << ">";
+    std::cin >> input;
+    
+    if (input == "ADD")
+    {
+        
+    }
+    
+    
+}
