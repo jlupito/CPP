951a93a7955d6357137f1c973614423de37b71d9
jlupito <jl.arthaud@protonmail.com>
2023-12-08 16:46:32 +0100

    cpp04 ex01 WIP

--- CPP_04/ex01/Brain.cpp
+++ CPP_04/ex01/Brain.cpp
@@ -1,37 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*   Brain.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jarthaud <jarthaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/04 14:56:55 by jarthaud          #+#    #+#             */
-/*   Updated: 2023/12/08 16:39:04 by jarthaud         ###   ########.fr       */
+/*   Created: 2023/12/08 16:20:37 by jarthaud          #+#    #+#             */
+/*   Updated: 2023/12/08 16:20:53 by jarthaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CAT_HPP
-# define CAT_HPP
-
-#include <iostream>
-#include <cmath>
-
-class Cat : public Animal {
-    
-public:
-
-	Cat( void );  // constructeur par defaut
-	Cat( Cat const &copy ); // constructeur de recopie
-	~Cat( void ); //destructeur
-	
-	Cat &operator=( const Cat &copy); // operateur d affectation
-
-	virtual void makeSound( void ) const;
-
-private:
-
-	Brain	*_brain;
-    
-};
-
-#endif
+#include "Brain.hpp"
--- CPP_04/ex01/Brain.hpp
+++ CPP_04/ex01/Brain.hpp
@@ -1,37 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Cat.hpp                                            :+:      :+:    :+:   */
+/*   Brain.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jarthaud <jarthaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/04 14:56:55 by jarthaud          #+#    #+#             */
-/*   Updated: 2023/12/08 16:39:04 by jarthaud         ###   ########.fr       */
+/*   Created: 2023/12/08 16:21:02 by jarthaud          #+#    #+#             */
+/*   Updated: 2023/12/08 16:34:02 by jarthaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CAT_HPP
-# define CAT_HPP
+#ifndef BRAIN_HPP
+# define BRAIN_HPP
 
 #include <iostream>
 #include <cmath>
 
-class Cat : public Animal {
-    
-public:
-
-	Cat( void );  // constructeur par defaut
-	Cat( Cat const &copy ); // constructeur de recopie
-	~Cat( void ); //destructeur
+class Brain {
 	
-	Cat &operator=( const Cat &copy); // operateur d affectation
+	protected:
+		
 
-	virtual void makeSound( void ) const;
-
-private:
-
-	Brain	*_brain;
-    
+	
 };
-
-#endif
