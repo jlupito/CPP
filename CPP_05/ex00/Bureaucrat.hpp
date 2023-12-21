0a3b6ba6cda95da60608538be3e76e72f24e8caa
jlupito <jl.arthaud@protonmail.com>
2023-12-21 10:49:29 +0100

    Started CPP05

--- CPP_05/ex00/Bureaucrat.hpp
+++ CPP_05/ex00/Bureaucrat.hpp
@@ -6,7 +6,21 @@
 /*   By: jarthaud <jarthaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 09:45:07 by jarthaud          #+#    #+#             */
-/*   Updated: 2023/12/20 16:34:28 by jarthaud         ###   ########.fr       */
+/*   Updated: 2023/12/21 10:44:33 by jarthaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <string>
+
+class Bureaucrat {
+	private:
+		unsigned int 	_grade;
+		std::string		_name;
+	public:
+		string 
+}
+
+#endif
