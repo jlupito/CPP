c4e32849dd5df92e74141bccb02f2df1f2304c57
jlupito <jl.arthaud@protonmail.com>
2024-01-05 09:43:44 +0100

    started c6ex00

--- CPP_06/ex00/main.cpp
+++ CPP_06/ex00/main.cpp
@@ -3,10 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jarthaud <jarthaud@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jlupito <jlupito@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 12:15:27 by jarthaud          #+#    #+#             */
-/*   Updated: 2024/01/04 12:15:28 by jarthaud         ###   ########.fr       */
+/*   Updated: 2024/01/05 09:34:18 by jlupito          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ScalarConverter.hpp"
+
+int main(int argc, char **argv) {
+    if(argc != 2)
+    {
+        std::cout << "Wrong number of arguments" << std::endl;
+        return 1;
+    }
+    ScalarConverter::convert(argv[1]);
+
+    return 0;
+}
--- CPP_06/ex00/scalarConverter.hpp
+++ CPP_06/ex00/scalarConverter.hpp
@@ -3,18 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   scalarConverter.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jarthaud <jarthaud@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jlupito <jlupito@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/04 12:15:32 by jarthaud          #+#    #+#             */
-/*   Updated: 2024/01/04 17:53:41 by jarthaud         ###   ########.fr       */
+/*   Updated: 2024/01/05 09:34:52 by jlupito          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string>
 
 class ScalarConverter {
+	
 	public:
 		static void convert( std::string& str );
+		static void     convert(const std::string& str);
+        static bool     isChar(std::string str);
+        static bool     isInt(std::string str);
+        static bool     isFloat(std::string str);
+        static bool     isDouble(std::string str);
+        static void    convertChar(char _char);
+        static void    convertInt(int _int);
+        static void    convertFloat(float _float, std::string str);
+        static void    convertDouble(double _double, std::string str);
+        static void    printInt(int _int);
+        static void    printFloat(float _float);
+        static void    printDouble(double _double);
+        static void    printChar(char _char);
+		
 	private:
 		ScalarConverter( void );  // constructeur par defaut
 		ScalarConverter( ScalarConverter const &rhs ); // constructeur de recopie
