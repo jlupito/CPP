202ffe72e87664e8d1ed2aa5d5420f87c16d7a75
jlupito <jl.arthaud@protonmail.com>
2024-01-08 17:55:56 +0100

    added CPP07ex01

--- CPP_07/ex01/iter.hpp
+++ CPP_07/ex01/iter.hpp
@@ -6,10 +6,22 @@
 /*   By: jarthaud <jarthaud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 17:44:18 by jarthaud          #+#    #+#             */
-/*   Updated: 2024/01/08 17:44:59 by jarthaud         ###   ########.fr       */
+/*   Updated: 2024/01/08 17:55:08 by jarthaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 #include <iostream>
 
+template< typename T >
+void iter(T *array, size_t len, void(*f)(T&)) {
+	if (!array or !len or !foo) {
+		std::cout << "Iter cannot be called." << std::endl;
+		return;
+	}
+	else {
+		for (int i = 0; i < len; i++) 
+			f(array[i]);
+	}
+	return;
+}
